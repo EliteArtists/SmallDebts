76abd245612e014d77938bd67f8d74b8ee703e20
EliteArtists <164914828+EliteArtists@users.noreply.github.com>
2025-05-23 19:13:54 +0100

    Update scripts.js
    the header background to solid white when you scroll.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-// Custom scripts will go here
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  if (window.scrollY > 50) {
+    header.style.backgroundColor = "white";
+  } else {
+    header.style.backgroundColor = "rgba(255, 255, 255, 0.7)";
+  }
+});
