38f674643a998627b8f2de435b4497f29c6b30f4
EliteArtists <164914828+EliteArtists@users.noreply.github.com>
2025-05-23 19:34:47 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,1 @@
-window.addEventListener("scroll", function () {
-  const header = document.querySelector("header");
-  if (window.scrollY > 50) {
-    header.style.backgroundColor = "white";
-  } else {
-    header.style.backgroundColor = "rgba(255, 255, 255, 0.7)";
-  }
-});
+
